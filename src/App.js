5b9fdd8842b07f0b856bc233335b28657d5c5189
shubhamsharma199782 <shubham.sharma199782@gmail.com>
2020-08-12 19:44:58 +0530

    initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Bord from "./components/Bord";
+import Heading from "./components/Heading";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="playground">
+      <Heading />
+      <Bord />
     </div>
   );
 }
